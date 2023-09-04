3822cb8712f4dcf3d29467c2808ca58640b71919
RafaCartaxo <rafael.cartaxo@hotmail.com>
2023-09-04 20:22:20 -0300

    User query Restful service

--- src/main/java/jr/api/rest/controller/IndexController.java
+++ src/main/java/jr/api/rest/controller/IndexController.java
@@ -1,23 +1,44 @@
 package jr.api.rest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jr.api.rest.model.Usuario;
+import jr.api.rest.repository.UsuarioRepository;
+
 @RestController /*Arquitetura*/
 @RequestMapping(value = "/usuario")
 public class IndexController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	
-	@GetMapping(value = "/", produces = "application/json") /*Serviço*/
-	public ResponseEntity init (@RequestParam (value = "nome", required = true, defaultValue = "Nome não informado!") String nome) {
+	/*Serviço Restful*/
+	@GetMapping(value = "/{id}", produces = "application/json") /*Serviço*/
+	public ResponseEntity<Usuario> init (@PathVariable (value = "id") Long id) {
 		
-		System.out.println("Parâmetro sendo recebido " + nome);
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
 		
-		return new ResponseEntity("REST Spring Boot, nome: " + nome, HttpStatus.OK);
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+	}
+	
+	
+	@GetMapping(value = "/", produces = "application/json")
+	public ResponseEntity<List<Usuario>> usuario (){
+		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
+		
+		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 	}
 	
 }
